33e1f66203075b81d53857d1665133bea63b8169
VijayKumarReddyTalakola <109469574+VijayKumarReddyTalakola@users.noreply.github.com>
2022-10-15 11:04:06 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+import DigiClock from "./components/DigiClock";
+export default function App() {
+  return (
+    <div className="App">
+      <DigiClock />
+    </div>
+  );
+}
